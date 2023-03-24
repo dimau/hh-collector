184c60ff4dd21e16c509d1006a9841cd0c4cc303
Dmitrii <dimau777@gmail.com>
2023-03-24 22:56:32 +0400

    feat(main): add options for getVacancies

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ func main() {
 	appAccessToken := flag.String("APP_ACCESS_TOKEN", "", "Access token for application registered in hh.ru")
 	flag.Parse()
 
+	// Creating a new REST API client for hh.ru
 	client := hh.NewClient(
 		&url.URL{
 			Scheme: "https",
@@ -20,6 +21,7 @@ func main() {
 		"dimau-app/1.0 (dimau777@gmail.com)",
 		*appAccessToken)
 
+	// Test information about app
 	appInfo, err := client.Me()
 	if err != nil {
 		fmt.Printf("Error: %v", err)
@@ -27,7 +29,16 @@ func main() {
 		fmt.Printf("App info: %+v", appInfo)
 	}
 
-	vacancies, err := client.GetVacancies()
+	// Get vacancies
+	var options = &hh.OptionsForGetVacancies{
+		Text:         "react",
+		SearchField:  "name",
+		Period:       2,
+		ItemsPerPage: 5,
+		PageNumber:   0,
+	}
+
+	vacancies, err := client.GetVacancies(options)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	} else {
