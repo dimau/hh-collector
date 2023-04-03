70d50bf59fe4a78c5dd5e9d6cefce27d4ad82d8c
Dmitrii <dimau777@gmail.com>
2023-04-03 21:30:04 +0400

    Make dockerization of the application

--- go.mod
+++ go.mod
@@ -2,6 +2,11 @@ module github.com/dimau/job-collector-go
 
 go 1.20
 
-require github.com/dimau/go-hh-client v0.0.0-20230323172844-a677787cf23f // indirect
+require (
+	github.com/dimau/go-hh-client v0.0.0-20230324185551-9b0df7922fa2 // indirect
+	github.com/ijustfool/docker-secrets v0.0.0-20191021062307-b25ea5007562 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/rabbitmq/amqp091-go v1.8.0 // indirect
+)
 
-replace github.com/dimau/go-hh-client => ../go-hh-client
+//replace github.com/dimau/go-hh-client => ../go-hh-client
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"github.com/dimau/go-hh-client"
+	"github.com/ijustfool/docker-secrets"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
 	"net/url"
+	"time"
 )
 
 func main() {
 	// Get parameters of launching for application
-	appAccessToken := flag.String("APP_ACCESS_TOKEN", "", "Access token for application registered in hh.ru")
-	flag.Parse()
+	//appAccessToken := flag.String("APP_ACCESS_TOKEN", "", "Access token for application registered in hh.ru")
+	//flag.Parse()
+
+	// Get Docker secrets
+	dockerSecrets, _ := secrets.NewDockerSecrets("")
+	rabbitMQUser, _ := dockerSecrets.Get("rabbitmq_user")
+	rabbitMQPass, _ := dockerSecrets.Get("rabbitmq_pass")
+	appAccessToken, _ := dockerSecrets.Get("hh_api_token")
 
 	// Creating a new REST API client for hh.ru
 	client := hh.NewClient(
@@ -19,7 +29,7 @@ func main() {
 			Host:   "api.hh.ru",
 		},
 		"dimau-app/1.0 (dimau777@gmail.com)",
-		*appAccessToken)
+		appAccessToken)
 
 	// Test information about app
 	appInfo, err := client.Me()
@@ -43,5 +53,55 @@ func main() {
 		fmt.Printf("Error: %v", err)
 	} else {
 		fmt.Printf("Vacancies: %+v", vacancies)
+	}
+
+	/* Connect to RabbitMQ server */
+
+	// Creating of the connection
+	connectionURL := fmt.Sprintf("amqp://%v:%v@rabbitmq:5672/", rabbitMQUser, rabbitMQPass)
+	conn, err := amqp.Dial(connectionURL)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	// Creating of the channel
+	rabbitChannel, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer rabbitChannel.Close()
+
+	// Create or (if it's already exist) just get HeadHunter queue
+	args := make(amqp.Table)
+	args["x-message-ttl"] = int32(86400000)
+	q, err := rabbitChannel.QueueDeclare(
+		"HeadHunter", // name
+		true,         // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		args,         // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	// Create a context
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Publish received message to the queue Telegram in RabbitMQ
+	body := "Hello World from Head Hunter!"
+	err = rabbitChannel.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		},
+	)
+	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
 	}
 }
